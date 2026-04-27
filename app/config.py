c6c4a4dec833335cab9a2d1dbf9ca284346907d7
AnthonySoftArch <anthony.guaman@softarch.com.ec>
2026-04-27 15:31:19 -0500

    cambio base de datos

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_session_key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev_jwt_secret")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-    MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "horarios_db_2")
+    MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "horarios_db")
     DEBUG = True
